41db7024bbcddd0715ef8a44975841dd0ec638f7
Bleddian <120186496+bleddian@users.noreply.github.com>
2023-02-02 16:53:59 +0700

    Adding Skills and Projects (React Redux)

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,18 @@
+const redditApp = document.getElementsByClassName("reddit_app")[0];
+const redditAppFigure = document.getElementsByClassName("reddit_app_figure")[0];
+
+redditAppFigure.style.display = "none";
+
+const displayRedditAppFigure = () => {
+    if (redditAppFigure.style.display === "none") {
+        redditAppFigure.style.display = "block";
+    } else {
+        redditAppFigure.style.display = "none";
+    }
+}
+
+redditApp.addEventListener("click", displayRedditAppFigure);
+
 const javascriptSyntax = document.getElementsByClassName("javascript_syntax")[0];
 const javascriptSyntaxFigure = document.getElementsByClassName("javascript_syntax_figure")[0];
 
